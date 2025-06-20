4254f72cef7b2ee9cfc048378a144a40ab132d03
Simone Gentili <sensorario@gmail.com>
2025-06-20 13:25:52 +0200

    fix(window): adjust window size

--- electron/main.cjs
+++ electron/main.cjs
@@ -66,7 +66,7 @@ ipcMain.handle("get-best-move", (_, fen) => {
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
+    width: 900,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
